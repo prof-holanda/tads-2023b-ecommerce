82ce2025a5e77be61bc0a6fcac364ce559d1a8b6
Bruno R. Holanda <rf1792@ucdb.br>
2023-10-24 20:01:27 -0400

    ajustado class ProdutoCsvReader

---
+++ src/br/com/ecommerce/ProdutoCsvReader.java
@@ -0,0 +1,47 @@
+package br.com.ecommerce;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProdutoCsvReader {
+    public static Map<Integer, Produto> carregarTodos() {        
+        Map<Integer, Produto> produtos = new HashMap<>();
+        
+        try{
+            
+            File arquivoProdutos = new File(
+                "C:\\Users\\rf1792\\projetos\\ecommerce\\src\\br\\com\\ecommerce\\produtos.csv"
+            );
+            CsvReader reader = new CsvReader(arquivoProdutos, ';');
+            
+            reader.next();
+            reader.next();
+            reader.next();
+            
+            while (reader.hasNext()) {
+                Produto p = new Produto(
+                        reader.nextInt(),
+                        reader.next(),
+                        reader.nextInt()
+                );
+                
+                produtos.put(p.getId(), p);
+            }
+            
+            
+        } catch( IOException e) {
+            e.printStackTrace();
+        }
+        
+        return produtos;
+    }
+    
+    public static void main(String[] args) {
+        Map<Integer, Produto> produtos 
+                = ProdutoCsvReader.carregarTodos();
+        
+        produtos.forEach((id, p) -> System.out.println(p));
+    }
+}
